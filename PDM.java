d65b68e5a6f4b54bbae5a1c5db911e23fed41aa8
sureshkumarreddy6790 <163987785+sureshkumarreddy6790@users.noreply.github.com>
2024-06-27 15:25:54 +0530

    first commit

---
+++ PDM.java
@@ -0,0 +1,34 @@
+/*
+Pre-defined Methods: 
+java.lang.Math : 
+      public static double sqrt(double a)
+	  public static double cbrt(double a)
+	  public static double ceil(double a)
+	  public static double floor(double a)
+	  public static double rint(double a)
+	  public static double pow(double a, double b)
+	  public static int round(float a)
+	  public static double random()
+	  public static int abs(int a)
+	  public static int max(int a, int b)..overloaded
+	  public static int min(int a, int b)..overloaded
+	  public static double log(double a)
+
+
+
+
+
+
+
+
+
+
+String
+Scanner
+Arrays
+Integer
+Float
+Double
+....
+Object
+*/
---
+++ interviewQuestionsMethods.java
@@ -0,0 +1,28 @@
+/*
+Method:Types of Method
+1. What do you mean by method?
+2. How do we create a method?
+3. What are the compulsory elements in method declaration?
+4. How do we call a method?
+5. What is a PDM?
+6. What are the disadvantages of PDM?
+7. What are the advantages of PDM?
+8. What is a UDM?
+9. What are the advantages of UDM?
+10. What are the disadvantages of a UDM?
+11. What do you mean by static method?
+12. What do you mean by non static method?
+13. What is the difference between SM and NSM?
+14. What is the use of return type in a method?
+15. What are the valid return types?
+16. What is the role of keyword 'void'?
+17. What is the role of keyword 'return'?
+18. Where do we use 'return' keyword in method?
+19. How many return statements can be used?
+20. What are the factors affecting return statement and return type?
+21. What do you mean by parameter?
+22. Why do we need parameters?
+23. Why do we need return type?
+24. When should we use method with return type of any data type?
+25...................
+*/
